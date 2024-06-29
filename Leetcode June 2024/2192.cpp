75b2e61b91da76acc165de910bfdd5ac6b07cd16
Ayush Kumar Dwivedi <acool4974@gmail.com>
2024-06-29 20:24:07 +0530

    Create 2192.cpp

---
+++ Leetcode June 2024/2192.cpp
@@ -0,0 +1,48 @@
+// You are given a positive integer n representing the number of nodes of a 
+// Directed Acyclic Graph (DAG). The nodes are numbered from 0 to n - 1 (inclusive).
+
+// You are also given a 2D integer array edges, where edges[i] = [fromi, toi] 
+// denotes that there is a unidirectional edge from fromi to toi in the graph.
+
+// Return a list answer, where answer[i] is the list of ancestors of the ith node, 
+// sorted in ascending order.
+
+// A node u is an ancestor of another node v if u can reach v via a set of edges.
+
+Example : 
+Input: n = 8, edgeList = [[0,3],[0,4],[1,3],[2,4],[2,7],[3,5],[3,6],[3,7],[4,6]]
+Output: [[],[],[],[0,1],[0,2],[0,1,3],[0,1,2,3,4],[0,1,2,3]]
+Explanation:
+The above diagram represents the input graph.
+- Nodes 0, 1, and 2 do not have any ancestors.
+- Node 3 has two ancestors 0 and 1.
+- Node 4 has two ancestors 0 and 2.
+- Node 5 has three ancestors 0, 1, and 3.
+- Node 6 has five ancestors 0, 1, 2, 3, and 4.
+- Node 7 has four ancestors 0, 1, 2, and 3.
+
+x-------------------------------------------x----------------x---------------------------------------------x
+
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> ans(n), directChild(n);
+        for (auto& e : edges) {
+            directChild[e[0]].push_back(e[1]);
+        }
+        for (int i = 0; i < n; i++) {
+            dfs(i, i, ans, directChild);
+        }
+        return ans;
+    }
+
+private:
+    void dfs(int x, int curr, vector<vector<int>>& ans, vector<vector<int>>& directChild) {
+        for (int ch : directChild[curr]) {
+            if (ans[ch].empty() || ans[ch].back() != x) {
+                ans[ch].push_back(x);
+                dfs(x, ch, ans, directChild);
+            }
+        }
+    }
+};
